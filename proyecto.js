b2d447f296f0054efee7cfe5dfcad4b2b1645632
Omar-Molares <144066971+Omar-Molares@users.noreply.github.com>
2024-08-13 14:20:48 -0300

    Primera versión de Desencriptador de texto

---
+++ proyecto.js
@@ -0,0 +1,64 @@
+const textArea = document.querySelector(".text-area");
+const mensaje = document.querySelector(".mensaje");
+
+
+function btnEncriptar(){
+  const textoEncriptado = encriptar(textArea.value)
+  mensaje.value = textoEncriptado
+  textArea.value = "";
+  mensaje.style.backgroundImage = "none";
+  document.getElementById("btnCopy").style.visibility = "visible"; 
+  
+}
+
+function encriptar(stringEncriptada) {
+  let matrizCodigo = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]];
+  stringEncriptada = stringEncriptada.toLowerCase()
+  
+  for( let i = 0; i < matrizCodigo.length; i++) {
+    if(stringEncriptada.includes(matrizCodigo[i][0])) {
+      stringEncriptada = stringEncriptada.replaceAll(matrizCodigo[i][0], matrizCodigo[i][1])
+    }
+  }
+  return stringEncriptada
+}
+
+function btnDesencriptar(){
+  const textoEncriptado = desencriptar(textArea.value)
+  mensaje.value = textoEncriptado
+  textArea.value = "";
+  mensaje.style.backgroundImage = "none";
+}
+
+
+function desencriptar(stringDesencriptada) {
+  let matrizCodigo = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]];
+  stringDesencriptada = stringDesencriptada.toLowerCase()
+  
+  for( let i = 0; i < matrizCodigo.length; i++) {
+    if(stringDesencriptada.includes(matrizCodigo[i][1])) {
+      stringDesencriptada = stringDesencriptada.replaceAll(matrizCodigo[i][1], matrizCodigo[i][0])
+    }
+  }
+  return stringDesencriptada;
+}
+
+
+function myFunction() {
+  
+  // Get the text field
+  let copyText = document.getElementById("myTextarea");
+
+  // Select the text field
+  copyText.select();
+  copyText.setSelectionRange(0, 99999); // For mobile devices
+
+  // Copy the text inside the text field
+  navigator.clipboard.writeText(copyText.value);
+  copyText.value = "";
+  mensaje.style.backgroundImage = "url()";
+  document.getElementById("btnCopy").style.visibility = "hidden";
+  
+}
+
+
